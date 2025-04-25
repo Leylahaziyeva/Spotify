544ffd5376300feccb51966cd81a3aa82eb3acbf
Leyla Haziyeva <leylaih@code.edu.az>
2025-04-26 00:09:22 +0400

    Added Update&Delete

--- Spotify.UI/Program.cs
+++ Spotify.UI/Program.cs
@@ -17,6 +17,12 @@ while (true)
     Console.WriteLine("4. List Albums");
     Console.WriteLine("5. Add Music");
     Console.WriteLine("6. List Musics");
+    Console.WriteLine("7. Update Artist");
+    Console.WriteLine("8. Delete Artist");
+    Console.WriteLine("9. Update Album");
+    Console.WriteLine("10. Delete Album");
+    Console.WriteLine("11. Update Music");
+    Console.WriteLine("12. Delete Music");
     Console.WriteLine("0. Exit");
     Console.Write("Choice: ");
 
@@ -62,10 +68,12 @@ while (true)
                 ArtistId = artistId
             });
             break;
+
         case "4":
             foreach (var a in albumService.GetAll())
                 Console.WriteLine($"{a.Id}: {a.Title}, {a.ReleaseDate:yyyy}, by {a.Artist?.Name}");
             break;
+
         case "5":
             Console.Write("Title: ");
             var mtitle = Console.ReadLine();
@@ -84,10 +92,90 @@ while (true)
                 AlbumId = albumId
             });
             break;
+
         case "6":
             foreach (var m in musicService.GetAll())
                 Console.WriteLine($"{m.Id}: {m.Title}, {m.Album?.Title}, by {m.Album?.Artist?.Name}");
             break;
+
+        case "7":
+            Console.Write("Artist ID to update: ");
+            var artistUpdateId = int.Parse(Console.ReadLine());
+            var artistToUpdate = artistService.GetById(artistUpdateId);
+            if (artistToUpdate == null)
+            {
+                Console.WriteLine("Artist not found.");
+                break;
+            }
+            Console.Write("New Name: ");
+            artistToUpdate.Name = Console.ReadLine();
+            Console.Write("New Genres: ");
+            artistToUpdate.Genres = Console.ReadLine();
+            Console.Write("New Image URL: ");
+            artistToUpdate.ImageUrl = Console.ReadLine();
+            artistService.Update(artistToUpdate);
+            break;
+
+        case "8":
+            Console.Write("Artist ID to delete: ");
+            var artistDeleteId = int.Parse(Console.ReadLine());
+            artistService.Delete(artistDeleteId);
+            break;
+
+        case "9":
+            Console.Write("Album ID to update: ");
+            var albumUpdateId = int.Parse(Console.ReadLine());
+            var albumToUpdate = albumService.GetById(albumUpdateId);
+            if (albumToUpdate == null)
+            {
+                Console.WriteLine("Album not found.");
+                break;
+            }
+            Console.Write("New Title: ");
+            albumToUpdate.Title = Console.ReadLine();
+            Console.Write("New Release Date: ");
+            albumToUpdate.ReleaseDate = DateTime.Parse(Console.ReadLine());
+            Console.Write("New Genres: ");
+            albumToUpdate.Genres = Console.ReadLine();
+            Console.Write("New Cover Image URL: ");
+            albumToUpdate.CoverImageUrl = Console.ReadLine();
+            Console.Write("New Artist ID: ");
+            albumToUpdate.ArtistId = int.Parse(Console.ReadLine());
+            albumService.Update(albumToUpdate);
+            break;
+
+        case "10":
+            Console.Write("Album ID to delete: ");
+            var albumDeleteId = int.Parse(Console.ReadLine());
+            albumService.Delete(albumDeleteId);
+            break;
+
+        case "11":
+            Console.Write("Music ID to update: ");
+            var musicUpdateId = int.Parse(Console.ReadLine());
+            var musicToUpdate = musicService.GetById(musicUpdateId);
+            if (musicToUpdate == null)
+            {
+                Console.WriteLine("Music not found.");
+                break;
+            }
+            Console.Write("New Title: ");
+            musicToUpdate.Title = Console.ReadLine();
+            Console.Write("New Release Date: ");
+            musicToUpdate.ReleaseDate = DateTime.Parse(Console.ReadLine());
+            Console.Write("New Genre: ");
+            musicToUpdate.Genre = Console.ReadLine();
+            Console.Write("New Album ID: ");
+            musicToUpdate.AlbumId = int.Parse(Console.ReadLine());
+            musicService.Update(musicToUpdate);
+            break;
+
+        case "12":
+            Console.Write("Music ID to delete: ");
+            var musicDeleteId = int.Parse(Console.ReadLine());
+            musicService.Delete(musicDeleteId);
+            break;
+
         case "0":
             return;
     }
